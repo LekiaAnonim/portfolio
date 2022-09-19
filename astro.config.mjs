d74ec7e4fa5396e2f23cde2fe205d4d336588601
Lekia Prosper <63610232+LekiaAnonim@users.noreply.github.com>
2022-09-19 04:41:09 +0100

    created netlify branch for deploy in netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import netlify from "@astrojs/netlify/functions";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://lekiaanonim.github.io',
-    base: '/portfolio',
+  output: "server",
+  adapter: netlify()
 });

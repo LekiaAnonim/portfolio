241bbababc4fe1997f922862c339df7089e9a279
Lekia Prosper <63610232+LekiaAnonim@users.noreply.github.com>
2022-09-19 02:56:50 +0100

    my first commit

---
+++ public/main.js
@@ -0,0 +1,23 @@
+
+
+
+// import mojs from '/node_modules/@mojs/core';
+
+// const burst = new mojs.Burst({
+//   radius:   { 0: 100 },
+//   count:    10,
+//   children: {
+//     shape:      'polygon',
+//     points:     5,
+//     fill:       { 'cyan' : 'yellow' },
+//     rotate:      { 360: 0 },
+//     duration:   2000,
+//     delay:      'stagger( rand(0, 200) )'
+//   }
+// });
+// const ele_burst = document.querySelectorAll('span');
+// Array.from(ele_burst).forEach(function(ele){
+// 	ele.addEventListener('mouseover', function(e){
+// 		burst.replay();
+// 	});
+// });
